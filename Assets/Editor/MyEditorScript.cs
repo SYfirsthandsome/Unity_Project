2405d8f37bb2efba8105580f7c887f9afb4e54d6
WJ <WJ@shimaoyedeMacBook-Pro.local>
2017-03-02 14:34:25 +0800

    第一次提交

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class MyEditorScript {
+
+
+	static void MyMethod(){
+	
+	
+		string executeMethod = "MyEditorScript.MyMethod";
+
+		foreach (string arg in System.Environment.GetCommandLineArgs()) {
+		
+		
+			Debug.Log (arg);
+		}
+
+
+	
+	}
+
+
+
+
+
+}
